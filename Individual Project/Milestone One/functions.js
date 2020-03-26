0915b6896fd341288e5d486e32d1c5173b858269
Sarah Guthrie <sarah.guthrie16@stjohns.edu>
2020-03-25 23:44:24 -0400

    Alerts encourgament

--- Individual Project/Milestone One/functions.js
+++ Individual Project/Milestone One/functions.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded',
 function(){
   document.getElementById("quiz_select_submit").onclick = chose;
   document.getElementById("quiz_one_submit").onclick = encouragment;
-
+  document.getElementById("quiz_one_submit").onclick = score;
+  document.getElementById("quiz_two_submit").onclick = encouragment;
 })
 
 function chose(){
@@ -14,17 +15,20 @@ function chose(){
     document.getElementById("quiz2").style.display = 'block';
   }
 }
-//
-// function score(){
-//   var counter = 0;
-//   if (document.getElementById("firstname").value == "Sarah") {
-//     counter = counter + 1
-//     document.getElementById("score").innerHTML = `Score: ${counter}`
-//   }
-// }
-// //
-// function encouragment(){
-//   var encouragment = ["Goodjob", "Welldone", "Keep It Going"]
-//   var randomEncourgment = encouragment[Math.floor(Math.random()*encouragment.length)];
-//   alert(`${randomEncourgment}`;
-// }
+
+function score(){
+  var counter = 0;
+  if (document.getElementById("firstname").value == "Sarah") {
+    counter = counter + 1
+    document.getElementById("score").innerHTML = `Score: ${counter}`
+    alert(counter);
+  }
+}
+
+function encouragment(){
+  var encouragment = ["Goodjob", "Welldone", "Keep It Going"]
+  var randomEncourgment = encouragment[Math.floor(Math.random()*encouragment.length)];
+  alert(randomEncourgment);
+
+  // document.getElementById("encouragment").style.display = 'block';
+}
